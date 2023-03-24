031e7f0f36e41e6ddb7b93bfc676ef3882c0ca24
Martin <hengwujun128@gmail.com>
2023-03-24 17:31:10 +0800

    feat: 集成 uni-UI 到当前项目中

--- src/pages/test2/index.vue
+++ src/pages/test2/index.vue
@@ -4,48 +4,70 @@
     <!-- <u-navbar back-text="返回" title="剑未配妥，出门已是江湖"></u-navbar> -->
     <uni-nav-bar
       dark
-      title="导航栏组件"
+      title="当前设备"
       :fixed="true"
       shadow
-      background-color="#007AFF"
-      status-bar
-      left-icon="left"
-      left-text="返回"
+      left-icon="gear"
+      left-text=""
+      right-icon="settings"
+      @clickLeft="goHome"
+      @clickRight="bootstrap"
     ></uni-nav-bar>
 
-    <!-- 正文内容 -->
-    <!-- <uni-list class="list">
-      <uni-list-item class="item">
-        <u-cell-item icon="" title="初始化蓝牙适配器">
-          <u-button type="primary" class="btn" size="medium" @click="bootstrap">初始化蓝牙适配器</u-button>
-        </u-cell-item>
-      </uni-list-item>
-      <uni-list-item class="item">
-        <u-cell-item icon="" title="开始搜寻附近设备">
-          <u-button type="primary" class="btn" size="medium" @click="startDiscovery">开始搜寻附近设备</u-button>
-        </u-cell-item>
-      </uni-list-item>
-    </uni-list> -->
-    <!-- <u-button type="primary" class="btn" size="medium" @click="bootstrap">初始化蓝牙适配器</u-button>
-    <u-button type="primary" class="btn" size="medium" @click="startDiscovery">开始搜寻附近设备</u-button>
-    <u-button type="primary" class="btn" size="medium" @click="getServices">获取蓝牙设备服务</u-button>
-    <u-button type="primary" class="btn" size="medium" @click="getCharacteristics">获取特征值</u-button>
-    <u-button type="danger" class="btn" size="medium" @click="stop">停止</u-button> -->
-
-    <scroll-view scroll-y class="box">
-      <view class="item" v-for="item in blueDeviceList" :key="item.deviceId" @click="connect(item)">
-        <view>
-          <text>id: {{ item.deviceId }}</text>
-        </view>
-        <view>
-          <text>name: {{ item.name }}</text>
-        </view>
+    <uni-section title="第一步: 初始化蓝牙适配器" type="line" top="20">
+      <view class="example-body box">
+        <button class="button" type="primary" @click="bootstrap">
+          <text class="button-text">初始化</text>
+        </button>
       </view>
-    </scroll-view>
+    </uni-section>
+
+    <uni-section title="第二步:开始搜寻附近设备, 返回设备列表" type="line" top="20">
+      <view class="example-body box">
+        <button class="button" type="primary" @click="startDiscovery">
+          <text class="button-text">开始搜寻</text>
+        </button></view
+      >
+    </uni-section>
+    <uni-section title="第三步:获取指定蓝牙设备服务" type="line" top="20">
+      <view class="example-body box">
+        <button class="button" type="primary" @click="getServices">
+          <text class="button-text">获取蓝牙设备服务</text>
+        </button>
+      </view>
+    </uni-section>
+    <uni-section title="第四步:获取指定蓝牙设备服务的特征值" type="line" top="20">
+      <view class="example-body box">
+        <button class="button" type="primary" @click="getServices">
+          <text class="button-text">获取特征值</text>
+        </button>
+      </view>
+    </uni-section>
+    <uni-section title="停止搜索:" type="line" top="20">
+      <view class="example-body box">
+        <button class="button button-error" type="danger" @click="stop">
+          <text class="button-text button-text__error">停止搜索</text>
+        </button>
+      </view>
+    </uni-section>
+    <!-- 正文内容 -->
+    <uni-list v-if="blueDeviceList">
+      <uni-list-item title="" clickable v-for="item in blueDeviceList" :key="item.deviceId" @click="connect(item)">
+        <template #body>
+          <view>
+            <text>id: {{ item.deviceId }}</text>
+          </view>
+          <view>
+            <text>name: {{ item.name }}</text>
+          </view>
+        </template>
+      </uni-list-item>
+    </uni-list>
   </view>
 </template>
 
 <script setup lang="ts">
+import { onBackPress } from '@dcloudio/uni-app'
 import { ref, Ref, onMounted, onBeforeUnmount } from 'vue'
 
 interface DEVICE {
@@ -65,11 +87,15 @@ function ab2hex(buffer: Iterable<number>) {
   return hexArr.join('')
 }
 
+const goHome = () => {
+  console.log('go home ---')
+}
 const bootstrap = () => {
   initBlueTooth()
 }
 const stop = () => {
   stopDiscovery()
+  console.log('blueDeviceList---', blueDeviceList.value)
 }
 
 // 获取指定设备,指定服务的特征值: - 根据 设备ID 和 服务ID。
@@ -154,18 +180,19 @@ const startDiscovery = () => {
   console.log('开始蓝牙搜索----')
   uni.startBluetoothDevicesDiscovery({
     // 以微信硬件平台的蓝牙智能灯为例，主服务的 UUID 是 FEE7。传入这个参数，只搜索主服务 UUID 为 FEE7 的设备
+    // 建议主要通过该参数过滤掉周边不需要处理的其他蓝牙设备
     // services: [], //['FEE7'], 要搜索的蓝牙设备主 service 的 uuid 列表,建议主要通过该参数过滤掉周边不需要处理的其他蓝牙设备。
-    services: ['FEE7'],
+    // services: ['FEE7'],
     success: (res) => {
       console.log('开始蓝牙搜索成功')
       console.log({ startBluetoothDevicesDiscovery: res })
       // 监听寻找到新设备的事件
-      uni.onBluetoothDeviceFound((devices) => {
-        // @ts-ignore
-        console.log('new device list has founded', devices[0].name)
-        console.dir(devices)
-        // @ts-ignore
-        blueDeviceList.value.push(devices[0])
+      uni.onBluetoothDeviceFound((data) => {
+        const { devices = [] } = data
+        // console.log('deviceId----name---:', devices[0].name)
+        if (devices[0].name.indexOf('Unknown') < 0) {
+          blueDeviceList.value.push(devices[0])
+        }
       })
     },
     fail: (err) => {
@@ -202,15 +229,52 @@ onBeforeUnmount(() => {
 })
 </script>
 
-<style lang="less" scoped>
+<style lang="scss" scoped>
 .container {
   padding: 8px;
 
-  .header {
+  @mixin flex {
+    /* #ifndef APP-NVUE */
     display: flex;
+    /* #endif */
+    flex-direction: row;
+  }
+
+  @mixin height {
+    /* #ifndef APP-NVUE */
+    height: 100%;
+    /* #endif */
+    /* #ifdef APP-NVUE */
+    flex: 1;
+    /* #endif */
+  }
+
+  .box {
+    @include flex;
+  }
+  .button {
+    @include flex;
     align-items: center;
-    .btn {
-      flex: 1;
+    justify-content: center;
+    flex: 1;
+    height: 35px;
+    margin: 0 5px;
+    border-radius: 5px;
+  }
+  .button-error {
+    color: #fff;
+    background-color: #fde2e2;
+  }
+
+  .example-body {
+    background-color: #fff;
+    padding: 10px 0;
+  }
+  .button-text {
+    color: #fff;
+    font-size: 12px;
+    &__error {
+      color: #f56c6c;
     }
   }
 }
