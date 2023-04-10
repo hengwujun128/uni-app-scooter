ec6cacc0ca05a76842df62fb06b99b35e9b67d38
Martin <hengwujun128@gmail.com>
2023-04-11 00:48:45 +0800

    feat: add main page

--- src/pages/main/main-old.vue
+++ src/pages/main/main-old.vue
@@ -27,22 +27,59 @@
     </view>
     <view class="body">
       <view class="dashBoard">
-        <uni-chart ref="chartRef" :option="option" @inited="inited" />
-        <view class="battery">电池</view>
-        <text>hello</text>
+        <!-- <uni-chart ref="chartRef" :option="option" @inited="inited" /> -->
+        <image src="/static/images/scooter/背景.png" class="back" mode="widthFix"></image>
+        <image class="arc" src="/static/images/scooter/圆弧.png" mode="widthFix"></image>
+        <image :style="pointStyle" class="point" src="/static/images/scooter/指针.png" mode="widthFix"></image>
+        <!-- 电池 -->
+        <view class="power">
+          <!-- <image
+            class="power-back power-back__first"
+            src="/static/images/scooter/power/back-1.png"
+            mode="widthFix"
+          ></image>
+          <image
+            class="power-back power-back__second"
+            src="/static/images/scooter/power/back-2.png"
+            mode="widthFix"
+          ></image> -->
+          <!-- 坑爹,直接使用带背景的电池即可 -->
+          <image class="power-battery" src="/static/images/scooter/power/0.png" mode="widthFix"></image>
+
+          <!-- 这里应该使用 background -->
+          <!-- <view class="battery-wrapper">
+            <image class="power-battery" src="/static/images/scooter/power/0.png" mode="widthFix"></image>
+          </view> -->
+          <!-- <image class="power-battery" src="/static/images/scooter/power/battery.png" mode="widthFix"></image> -->
+        </view>
       </view>
+
       <view class="actions"></view>
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref, Ref } from 'vue'
+import { ref, Ref, onMounted } from 'vue'
 import useDashBoard, { option } from './useDashBoard.ts'
 
 const chartRef: Ref | null = ref(null)
 
 const { inited } = useDashBoard(chartRef)
+const pointStyle = ref({})
+
+const setDegree = () => {
+  const deg = Math.floor(Math.random() * 100) + 1
+  pointStyle.value = {
+    transition: 'all linear 1s',
+    transform: `translate(-50%, -50%) rotate(${deg}deg)`
+  }
+}
+onMounted(() => {
+  setInterval(() => {
+    setDegree()
+  }, 3000)
+})
 </script>
 
 <style lang="scss" scoped>
@@ -85,10 +122,66 @@ const { inited } = useDashBoard(chartRef)
       margin: 0 auto;
       position: relative;
     }
-    .battery {
+
+    .back {
       position: absolute;
-      bottom: 10rpx;
       left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .arc {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .point {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .power {
+      position: relative;
+      left: 50%;
+      top: 70%;
+      transform: translate(-50%, -50%);
+      margin: 0 auto;
+    }
+    .power-back {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .power-back__first {
+    }
+    .power-back__second {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .power-battery {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      width: 300rpx;
+      // margin-left: -103rpx;
+      // margin-top: -70rpx;
+    }
+    //
+    .battery-wrapper {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      width: 206rpx;
+      height: 96rpx;
+      margin-left: -103rpx;
+      margin-top: -70rpx;
+      background: url(/static/images/scooter/power/battery.png) no-repeat;
+      background-size: contain;
     }
   }
 }
--- src/pages/test1/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>this is tab1 page</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
